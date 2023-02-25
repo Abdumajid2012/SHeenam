6f00e8120b7986e71a124b02e629f550f599f408
Abdumajid2012 <90825804+Abdumajid2012@users.noreply.github.com>
2023-02-25 22:02:01 +0500

    CONFIG: Register Broker

--- Sheenam.Api/Brokers/Storages/IStorageBroker.Guests.cs
+++ Sheenam.Api/Brokers/Storages/IStorageBroker.Guests.cs
@@ -3,10 +3,13 @@
 // Free To Use To Find Comfort and Peace
 //==================================================
 
+using System.Threading.Tasks;
+using Sheenam.Api.Models.Foundations.Guests;
+
 namespace Sheenam.Api.Brokers.Storages
 {
     public partial interface IStorageBroker
     {
-
+        ValueTask <Guest> InsertGuestAsync(Guest guest);
     }
 }
